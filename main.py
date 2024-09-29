aa782214da1e745753b58d71fb966c088cc62e37
Tebmer <932208373@qq.com>
2024-09-30 00:25:17 +0800

    Add LICENSE

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-# Copyright 2023 PAL Authors. All rights reserved.
+# Copyright 2022 PAL Authors. All rights reserved.
+# Copyright 2024 Re2 Authors. All rights reserved.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
